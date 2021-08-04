2075e0497389e4f1882ce1c2272dc758e29b2833
thuyvie <thuyvy1235789@gmail.com>
2021-08-04 15:11:14 +0700

    test

--- vd/src/main/java/module-info.java
+++ vd/src/main/java/module-info.java
@@ -1,6 +1,9 @@
 module com.quyen.vd {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
+    requires javafx.base;
+    requires javafx.graphics;
 
     opens com.quyen.vd to javafx.fxml;
     exports com.quyen.vd;
